321e1807ade8d4b6a5ed71fd8c9f27b7f3795a08
Mia Masmela <mimimasmela@outlook.com>
2025-11-04 13:59:08 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import babel from "vite-plugin-babel";
-import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const ReactCompilerConfig = { /* ... */ };
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRouter(),
-    babel({
-      filter: /\.[jt]sx?$/,
-      babelConfig: {
-        presets: ["@babel/preset-typescript"], // if you use TypeScript
-        plugins: [
-          ["babel-plugin-react-compiler", ReactCompilerConfig],
-        ],
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
